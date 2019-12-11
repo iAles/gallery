438a22a75a0c4fc111506ae1bbc06bb79ed38c4e
Aliaksandr Kazakou <a.kazakou@gmail.com>
2019-12-11 13:41:24 +0100

    add array

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,17 @@
-const image = 'kotek.png';
-console.log(image);
+const image1 = "https://picsum.photos/300/150?random=1";
+const image2 = "https://picsum.photos/300/150?random=2";
+const image3 = "https://picsum.photos/300/150?random=3";
+
+const images = [image1, image2, image3];
+
+for (const image of images) {
+    console.log("To jest obrazek" + image);
+    console.log('PK');
+}
+
+for (let i = 0; i < images.length; i++) {
+    const image = images[i];
+    console.log ("To jest obrazek" + image);
+    console.log ('PK');
+}
+
