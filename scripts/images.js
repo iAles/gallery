3f60a5872b92d67647ec14ffc7bf92b5285b8334
Aliaksandr Kazakou <a.kazakou@gmail.com>
2019-12-11 15:16:20 +0100

    move images to images.js

---
+++ scripts/images.js
@@ -0,0 +1,5 @@
+const images = [
+    "https://picsum.photos/id/40/300/150",
+    "https://picsum.photos/id/50/300/150",
+    "https://picsum.photos/id/60/300/150",
+];
--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,26 @@
-const image1 = "https://picsum.photos/id/40/300/150";
-const image2 = "https://picsum.photos/id/50/300/150";
-const image3 = "https://picsum.photos/id/60/300/150";
-
-const images = [image1, image2, image3];
-
-// for (const image of images) {
+for (const image of images) {
 //     console.log("To jest obrazek" + image);
 //     console.log('PK');
-// }
+}
 
-// for (let i = 0; i < images.length; i++) {
-//     const image = images[i];
-//     console.log("To jest obrazek" + image);
-//     console.log('PK');
-// }
+for (let i = 0; i < images.length; i++) {
+    // const image = images[i];
+    // console.log("To jest obrazek" + image);
+    // console.log('PK');
+}
 
 // const isEmpty = (images.length === 0);
 // const isEmpty2 = (images.length == 0);
 
-// function displayImages(params) {
+function displayImages(params) {
 //     console.log('Liczba obrazków ' + images.length);
 //     console.log('Pierwszy element listy ' + images[0]);
 //     console.log('Drugi element listy ' + images[1]);
-// }
+}
+
 // displayImages();
 
-// function displayLastElment() {
+function displayLastElment() {
 //     console.log('Ostatni element listy ' + images[images.length - 1]);
 //     console.log('/  ' + 23442423 / 235);
 //     console.log('*  ' + 23442423 * 235);
@@ -33,7 +28,7 @@ const images = [image1, image2, image3];
 //     console.log('-  ' + 23442423 - 235);
 //     console.log('** ' + 23442423 **5);
 
-// }
+}
 // displayLastElment();
 
 function renderPhoto(url) {
