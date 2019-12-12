2b4afdcf93e403c1a7d40dd3a18407ea8f738bc1
Kazakou, Aliaksandr (WAW-MWG) <Aliaksandr.Kazakou@mccannworldgroup.pl>
2019-12-12 12:11:31 +0100

    Convert string to object

--- scripts/images.js
+++ scripts/images.js
@@ -1,5 +1,17 @@
 const images = [
-    "https://picsum.photos/id/40/300/150",
-    "https://picsum.photos/id/50/300/150",
-    "https://picsum.photos/id/60/300/150",
+    {
+        url: "https://picsum.photos/id/40/300/150",
+        title: "KOTEŁ",
+        author: 'ja'
+    },
+    {
+        url: "https://picsum.photos/id/50/300/150",
+        title: "Drank bird",
+        author: 'Zenek'
+    },
+    {
+        url: "https://picsum.photos/id/60/300/150",
+        title: "iMac",
+        author: 'On'
+    }
 ];
--- scripts/main.js
+++ scripts/main.js
@@ -1,59 +1,94 @@
-for (const image of images) {
-    //     console.log("To jest obrazek" + image);
-    //     console.log('PK');
-}
+// for (const image of images) {
+//     //     console.log("To jest obrazek" + image);
+//     //     console.log('PK');
+// }
 
-for (let i = 0; i < images.length; i++) {
-    // const image = images[i];
-    // console.log("To jest obrazek" + image);
-    // console.log('PK');
-}
+// for (let i = 0; i < images.length; i++) {
+//     // const image = images[i];
+//     // console.log("To jest obrazek" + image);
+//     // console.log('PK');
+// }
 
 // const isEmpty = (images.length === 0);
 // const isEmpty2 = (images.length == 0);
 
-function displayImages(params) {
-    //     console.log('Liczba obrazków ' + images.length);
-    //     console.log('Pierwszy element listy ' + images[0]);
-    //     console.log('Drugi element listy ' + images[1]);
-}
+// function displayImages(params) {
+//     //     console.log('Liczba obrazków ' + images.length);
+//     //     console.log('Pierwszy element listy ' + images[0]);
+//     //     console.log('Drugi element listy ' + images[1]);
+// }
 
 // displayImages();
 
-function displayLastElment() {
-    //     console.log('Ostatni element listy ' + images[images.length - 1]);
-    //     console.log('/  ' + 23442423 / 235);
-    //     console.log('*  ' + 23442423 * 235);
-    //     console.log('+  ' + 23442423 + 235);
-    //     console.log('-  ' + 23442423 - 235);
-    //     console.log('** ' + 23442423 **5);
+// function displayLastElment() {
+//     //     console.log('Ostatni element listy ' + images[images.length - 1]);
+//     //     console.log('/  ' + 23442423 / 235);
+//     //     console.log('*  ' + 23442423 * 235);
+//     //     console.log('+  ' + 23442423 + 235);
+//     //     console.log('-  ' + 23442423 - 235);
+//     //     console.log('** ' + 23442423 **5);
 
-}
+// }
 // displayLastElment();
 
-function renderPhoto(url) {
-    const img = document.createElement('img');
-    img.src = url;
-    const main = document.querySelector('main');
-    main.append(img);
+function renderPhoto(photo) {
+    const $card = document.createElement('div');
+    $card.classList.add('card', 'col-3', 'mx-4');
+
+    const $img = document.createElement('img');
+    $img.src = photo.url;
+
+    const $body = document.createElement('div');
+    $body.classList.add('card-body');
+
+    const $title = document.createElement('h5');
+    $title.classList.add('card-title');
+    $title.textContent = photo.title;
+
+    // Group HTML Elements
+    $card.append($img);
+    $card.append($body);
+    $body.append($title);
+
+    // Render
+    const $main = document.querySelector('main');
+    $main.append($card);
 }
 
-// renderPhoto('https://picsum.photos/id/10/300/150');
-// renderPhoto('https://picsum.photos/id/10/300/150');
-// renderPhoto('https://picsum.photos/id/10/300/150');
+/**
+ * @typedef {Object} Photo
+ * @property {string} url
+ * @property {string} title
+ * @property {string} author
+ *
+ * @typedef {Array<Photo>} Photos
+ */
 
+/**
+ * @param {Photos} photos
+ */
 function displayPhotos(photos) {
-    console.log(photos);
     for (const photo of photos) {
         renderPhoto(photo);
     }
 }
 
+// Uruchomienie funkcji
+// displayImages();
+// displayLastElement();
 
-// displayPhotos(images);
+// renderPhoto('https://picsum.photos/id/10/300/150');
+// renderPhoto('https://picsum.photos/id/20/300/150');
+// renderPhoto('https://picsum.photos/id/30/300/150');
 
 function displayMessage(message) {
     const main = document.querySelector('main');
+    // main.textContent = message;
+
+    // Concatenate Strings
+    // main.innerHTML = '<p class="alert alert-info text-center">' + message + '</p>';
+
+    // Template Literal Strings + Interpolacja
     main.innerHTML = `
         <p class="alert alert-info text-center">
         ${message}
@@ -61,43 +96,29 @@ function displayMessage(message) {
     `;
 }
 
-// const canDisplayPhotos = prompt('Czy chciałbyś wyświetlić zdjęcia?');
-
 function shouldDisplayPhotos() {
-    // const answer = prompt ('Czy chciałbyś wyświetlić zdjęcia?');
-    // return (answer.toLowerCase() === 'yes');
-}
-
-function isPhotosEmpty() {
-    if (images.length != 0) {
-        console.log('zdjęcia istnieją');
-        return (true);
-    }
-
+    const answer = prompt('Czy chcesz wyświetlić zdjęcia?');
+    return (answer.toLowerCase() === 'tak');
 }
 
 function isEmpty() {
-    return (images.length === 0);
+    return images.length === 0;
 }
 
 loader.show();
 
-
 if (!isEmpty()) {
     console.log('zdjęcia istnieją');
     displayPhotos(images);
-
 } else {
-    console.log('brak zdjęć');
-    displayMessage('brak zdjęć')
-
+    console.log('zdjęcia nie istnieją');
+    displayMessage('Nie ma zdjęć');
 }
 
-setTimeout (function () {
-    console.log('chowamy loadera');
-    // @ts-ignore
+setTimeout(function () {
+    console.info('chowamy loaderka');
     loader.hide();
-}, 1500);
+}, 1500); // 1.5s
 
 
 
